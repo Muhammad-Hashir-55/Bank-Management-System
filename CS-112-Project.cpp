7483a35c5d13403b2b21da4963aa842d002d644f
Muhammad-Hashir-55 <167025341+Muhammad-Hashir-55@users.noreply.github.com>
2024-04-14 23:46:43 +0500

    project file

---
+++ CS-112-Project.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    cout <<"this is just testing the output and publishing this file for github repository"<<endl;
+    cout <<"testing line "<<endl;
+return 0;
+}
